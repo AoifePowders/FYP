514fda921a29558ac0c88fdf212ea36047bb787e
Aoife Powders <c00218374@itcarlow.ie>
2019-11-04 23:25:34 +0000

    Bat hitting ball

---
+++ SteamVR/Assets/Scripts/BatCapsule.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+//creates a capsule to follow the bat
+public class BatCapsule : MonoBehaviour
+{
+    [SerializeField]
+    private BatCapsuleFollower batCapsuleFollowerPrefab;
+
+    private void SpawnBatCapsuleFollower()
+    {
+        var follower = Instantiate(batCapsuleFollowerPrefab);
+        follower.transform.position = transform.position;
+        follower.SetFollowTarget(this);
+    }
+
+    private void Start()
+    {
+        SpawnBatCapsuleFollower();
+    }
+}
---
+++ SteamVR/Assets/Scripts/BatCapsuleFollower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BatCapsuleFollower : MonoBehaviour
+{
+    private BatCapsule _batFollower;
+    private Rigidbody rigidbody;
+    private Vector3 velocity;
+
+    [SerializeField]
+    private float sensitivity = 100f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 destination = _batFollower.transform.position;
+        rigidbody.transform.rotation = transform.rotation;
+
+        velocity = (destination - rigidbody.transform.position) * sensitivity;
+
+        rigidbody.velocity = velocity;
+        transform.rotation = _batFollower.transform.rotation;
+    }
+
+    public void SetFollowTarget(BatCapsule batFollower)
+    {
+        _batFollower = batFollower;
+    }
+}
