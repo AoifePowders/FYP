82d087d5f08482cfd1db4e841363758ce1a29334
Aoife Powders <c00218374@itcarlow.ie>
2020-03-25 15:01:17 +0000

    building game

---
+++ SteamVR/Assets/BackPouchCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackPouchCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Bat")
+        {
+            other.attachedRigidbody.isKinematic = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Bat")
+        {
+            other.attachedRigidbody.isKinematic = false;
+        }
+    }
+}
---
+++ SteamVR/Assets/MazeGameParticles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGameParticles : MonoBehaviour
+{
+
+    public ParticleSystem part;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Car")
+        {
+            part.Play();
+        }
+    }
+}
